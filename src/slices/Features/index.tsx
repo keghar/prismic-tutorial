284fd116c5909effe94e3e0ecb6a4cf8541dc180
keghar <108595664+keghar@users.noreply.github.com>
2024-01-05 16:36:02 -0600

    stuff

---
+++ src/slices/Features/index.tsx
@@ -0,0 +1,37 @@
+import { Content } from "@prismicio/client";
+import {
+  JSXMapSerializer,
+  PrismicRichText,
+  SliceComponentProps,
+} from "@prismicio/react";
+import Bounded from "@/components/Bounded";
+import Button from "@/components/Button";
+import Heading from "@/components/Heading";
+
+const components: JSXMapSerializer = {
+  heading2: ({ children }) => (
+    <Heading as="h2" size="md" className="text-center mb-12">
+      {children}
+    </Heading>
+  ),
+};
+
+/**
+ * Props for `Features`.
+ */
+export type FeaturesProps = SliceComponentProps<Content.FeaturesSlice>;
+
+/**
+ * Component for "Features" Slices.
+ */
+const Features = ({ slice }: FeaturesProps): JSX.Element => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}>
+      <PrismicRichText components={components} field={slice.primary.heading} />
+    </Bounded>
+  );
+};
+
+export default Features;
